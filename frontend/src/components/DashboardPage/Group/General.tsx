4262cc3556d2a418b1612fd9b5bc505b76cf1061
Bouhadda Mohammed Djaoued <bhdmeddjaoued@gmail.com>
2025-04-26 15:14:04 +0300

    better UI

--- frontend/src/components/DashboardPage/Group/General.tsx
+++ frontend/src/components/DashboardPage/Group/General.tsx
@@ -9,16 +9,16 @@ export default function General({ groupID }: GeneralProps) {
   return (
     <div
       dir="rtl"
-      className="rounded-xl mx-auto shadow-normal flex w-full px-4 py-4 gap-4"
+      className="rounded-xl mx-auto shadow-normal flex w-full px-4 py-4 gap-6"
     >
       <div className="flex-1 flex flex-col gap-2">
         <h1 className="text-2xl text-main font-bold">الشعبة {groupID}</h1>
-        <div className="flex flex-col lg:flex-row gap-1 items-center">
-          <p className="text-main text-sm font-semibold">
+        <div className="flex flex-col lg:flex-row justify-between items-center">
+          <p className="text-main text-[16px] font-semibold">
             عدد الطلاب المنتظمين:
             <AnimatedNumber from={0} to={24} className="" />
           </p>
-          <p className="text-main text-sm font-semibold">
+          <p className="text-main text-[16px] font-semibold">
             متوسط الغياب في الأسبوع:
             <AnimatedNumber from={0} to={4} className="" />
           </p>
